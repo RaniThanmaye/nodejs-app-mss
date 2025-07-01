44ef8556b9c2f6d0a6a59a2746b97020c617f992
RaniThanmaye <98078740+RaniThanmaye@users.noreply.github.com>
2025-07-01 19:32:19 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.171.209.223:9980/',
+  serverUrl: 'http://172.31.81.80:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_95f458af2e63dfdddb65b4f3e2ccfb04b3455ea9',
+	    'sonar.login': 'squ_77a4dee18792e6431b60841421468f811d825635',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
